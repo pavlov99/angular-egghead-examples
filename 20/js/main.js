a26bd4d10b0c1e48115f6f9bb576457344cc47b7
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-02-09 17:18:20 +0000

    add examples for 20th video

---
+++ 20/js/main.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module("phoneApp", []);
+
+app.controller("AppCtrl", function($scope){
+    $scope.leaveVoicemail = function(number, message){
+        alert("Number: ", + number + " said: " + message)
+    }
+})
+
+app.directive("phone", function(){
+    return {
+        restrict: "E",
+        scope: {
+            number: "@",
+            network: "=",
+            makeCall: "&"
+        },
+        template: '<div class="panel">Number: {{number}} Network:<select ng-model="network" ng-options="net for net in networks"></select></div>' +
+            '<input type="text" ng-model="valu">' +
+            '<div class="button" ng-click="makeCall({numnber: number, message: value})">Call home!</div></div>',
+        link: function(scope){
+            scope.networks = ["Verizon", "AT&T", "Sprint"];
+            scope.network = scope.networks[0]
+        }
+
+
+    }
+})
